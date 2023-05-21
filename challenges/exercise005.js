d8bf40168df0ffc413a70c0b24105eb28d37e034
Jennifer Orosun <orosunj@gmail.com>
2023-05-21 18:05:10 +0100

    findNeedle completed

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -54,7 +54,16 @@ export const arrShift = (arr) => {
 export const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+
+  for (const key in haystack) {
+    if (
+      typeof haystack[key] === "string" &&
+      haystack[key].toUpperCase().includes(searchTerm.toUpperCase())
+    ) {
+      return true;
+    }
+  }
+  return false;
 };
 
 export const getWordFrequencies = (str) => {
