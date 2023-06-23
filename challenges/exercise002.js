a2dd40ee13b29080bba245a3850bda7ef0ce1403
Jennifer Orosun <orosunj@gmail.com>
2023-06-23 23:51:50 +0100

    refactored exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,56 +1,50 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-    const fillingArray = [];
-	let fillingItem = '';
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  const fillingArray = [];
+  let fillingItem = "";
 
-	for (let i= 0; i < (sandwich["fillings"].length); i++) {
+  for (let i = 0; i < sandwich["fillings"].length; i++) {
+    fillingItem = sandwich["fillings"][i];
 
-			fillingItem = sandwich["fillings"][i];
+    fillingArray.push(fillingItem);
+  }
 
-			fillingArray.push(fillingItem);
-		}
-		
-	return fillingArray;
+  return fillingArray;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-    
-	if (person["city"] === "Manchester") {
-		return true;
-	} else {
-		return false;
-	}
+  if (person === undefined) throw new Error("person is required");
+  return person["city"] === "Manchester" ? true : false;
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	const peoplePerBus = 40;
-	let numberOfBuses = Math.ceil(( people / peoplePerBus ));
-	return (numberOfBuses);
+  if (people === undefined) throw new Error("people is required");
+  const PEOPLE_PER_BUS = 40;
+  let numberOfBuses = Math.ceil(people / PEOPLE_PER_BUS);
+  return numberOfBuses;
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	let numberOfSheep = 0;
-	for (let i= 0; i < arr.length; i++) {
-		if (arr[i] === "sheep") {
-			numberOfSheep=numberOfSheep+1;
-		} 
-	}
-	return numberOfSheep;
+  if (arr === undefined) throw new Error("arr is required");
+  let numberOfSheep = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "sheep") {
+      numberOfSheep = numberOfSheep + 1;
+    }
+  }
+  return numberOfSheep;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
+  if (person === undefined) throw new Error("person is required");
 
-	let mancCode = false;
-	const postcode = person["address"]["postCode"];
+  let mancCode = false;
+  const postcode = person["address"]["postCode"];
 
-	if  (postcode.slice(0,1) === "M" ) {
-		if (/[0-9]/.test(postcode.slice(1,2))) {
-			mancCode = true;
-		}
-	}
-    return mancCode;
+  if (postcode.slice(0, 1) === "M") {
+    if (/[0-9]/.test(postcode.slice(1, 2))) {
+      mancCode = true;
+    }
+  }
+  return mancCode;
 }
---
+++ test/exercise009.test.js
@@ -0,0 +1,5 @@
+describe("getMax", () => {
+    test("returns the max number", () => {
+      expect(getMax([2,3])).toBe("The maximum value is 3.");     
+    });
+  
